64f4d139cb877f5f316506c8c0284c0103e9bf14
jonathandwang <jdaegonw@gmail.com>
2021-04-28 14:20:58 -0500

    readded missing dependency for cascade delete

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :items
+  has_many :items, dependent: :destroy
 
   def total_spent
     items.reduce(0) { |total, item| total + item.price }
